8efd324627f8b9569467f79be826fc545193358a
Ahmad <33130717+amdnsr@users.noreply.github.com>
2023-05-20 02:00:38 +0530

    update process code to match ghostscript output structure

--- process_pdf_pdfminer_to_table_transformer.py
+++ process_pdf_pdfminer_to_table_transformer.py
@@ -7,10 +7,11 @@ from pdf_data_extraction import extract_character_info, extract_word_info
 if __name__ == "__main__":
     file_path = input("file_path: ")
     output_folder = input("output_folder: ")
-    os.makedirs(output_folder, exist_ok=True)
     filename = os.path.basename(file_path)
     folder_path = os.path.dirname(file_path)
     filename_wo_ext, ext = os.path.splitext(filename)
+    file_result_folder = os.path.join(output_folder, filename_wo_ext)
+    os.makedirs(file_result_folder, exist_ok=True)
     
     page_wise_data, all_outputs = extract_character_info(file_path)
     page_wise_words = extract_word_info(page_wise_data)
@@ -19,5 +20,5 @@ if __name__ == "__main__":
         page_words = page_wise_words[page_no]
         fill_char = "0"
         padded_page_no = "{:{fill_char}3}".format(page_no, fill_char=fill_char)
-        with open(os.path.join(output_folder, f"{filename_wo_ext}_{padded_page_no}_words.json"), "w", encoding="utf-8") as f:
+        with open(os.path.join(file_result_folder, f"{filename_wo_ext}_{padded_page_no}_words.json"), "w", encoding="utf-8") as f:
             f.write(json.dumps(page_words, indent=4))
