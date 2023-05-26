cf45eeb7ae60592addfd5b95e078ebd285f13617
Ahmad <33130717+amdnsr@users.noreply.github.com>
2023-05-26 13:09:02 +0530

    convert process code to a function

--- process_pdf_pdfminer_to_table_transformer.py
+++ process_pdf_pdfminer_to_table_transformer.py
@@ -4,9 +4,7 @@ import os
 from pdf_data_extraction import extract_character_info, extract_word_info
 
 
-if __name__ == "__main__":
-    file_path = input("file_path: ")
-    output_folder = input("output_folder: ")
+def create_words_json(file_path, output_folder):
     filename = os.path.basename(file_path)
     folder_path = os.path.dirname(file_path)
     filename_wo_ext, ext = os.path.splitext(filename)
@@ -22,3 +20,9 @@ if __name__ == "__main__":
         padded_page_no = "{:{fill_char}3}".format(page_no, fill_char=fill_char)
         with open(os.path.join(file_result_folder, f"{filename_wo_ext}_{padded_page_no}_words.json"), "w", encoding="utf-8") as f:
             f.write(json.dumps(page_words, indent=4))
+
+
+if __name__ == "__main__":
+    file_path = input("file_path: ")
+    output_folder = input("output_folder: ")
+    create_words_json(file_path, output_folder)
